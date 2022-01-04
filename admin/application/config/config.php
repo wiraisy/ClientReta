e2a276a941ed4feaf03086eb3f2c897ec08161c1
stephanus yogi <stephanusyogi12@gmail.com>
2022-01-04 13:31:49 +0700

    config

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ClientReta/admin';
+$config['base_url'] = 'https://shop.reta.co.id/admin';
 
 /*
 |--------------------------------------------------------------------------
