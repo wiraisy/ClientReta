2184e1f6df042916d0d665b4886294a1ab672d81
stephanus yogi <stephanusyogi12@gmail.com>
2022-01-09 22:45:02 +0700

    Test

---
+++ admin/application/controllers/Produklink.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Produklink extends CI_Controller {
+
+	public function index()
+	{
+		$data['title'] = "Produk Link";
+
+		$this->load->view('includes/header');
+		$this->load->view('v_produklink', $data);
+		$this->load->view('includes/footer');
+	}
+}
