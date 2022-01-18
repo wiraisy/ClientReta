e5f7aaa91eb425e7477c4acafcbf1e2a62636d39
stephanus yogi <stephanusyogi12@gmail.com>
2022-01-18 15:02:29 +0700

    test

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <title><?= $barTitle ?> | Reta Beauty Clinic</title>
   <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url() ?>assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="<?= base_url() ?>assets/img/favicon.png">
