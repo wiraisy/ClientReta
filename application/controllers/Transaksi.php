526923c1b545f430c257a15d85803c08f97ff0cc
stephanus yogi <stephanusyogi12@gmail.com>
2022-03-05 15:13:01 +0700

    New Progress

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -126,7 +126,7 @@ class Transaksi extends MY_Controller {
 		}
 
         // Get Data Penjualan Pasien
-		$url = 'https://api-reta.id/reta-api/Penjualan/getpenjualanbyid/'.$idpenjualan;
+		$url = 'https://api-reta.id/reta-api/Penjualan/getpenjualanbyid/'.$idpenjualan.'&sortDirection=ASC';
         $method = 'GET';
         $datapenjualan = $this->SendRequest($url, $method);
 
