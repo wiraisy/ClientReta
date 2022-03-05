5a33067dea7e0ade2959af8a2c97aaa32b7d699b
stephanus yogi <stephanusyogi12@gmail.com>
2022-03-05 15:31:18 +0700

    New Progress

--- admin/assets/js/custom/select-option-pesanan.js
+++ admin/assets/js/custom/select-option-pesanan.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
                     img.src = url;
                 }
             });
-            xhr.open("GET", "https://api-reta.id/reta-api/Produk/getImagebykodeid/" + selected_opt);
+            xhr.open("GET", "https://api-reta.id/reta-api/Produk/getproductimagebykodeid/" + selected_opt);
             xhr.setRequestHeader("Authorization", "Basic YWtiYXI6d2lyYWlzeQ==");
             xhr.responseType = 'blob';
             xhr.send();
@@ -201,7 +201,7 @@ $(document).ready(function() {
                     img.src = url;
                 }
             });
-            xhr.open("GET", "https://api-reta.id/reta-api/Produk/getImagebykodeid/" + selected_opt);
+            xhr.open("GET", "https://api-reta.id/reta-api/Produk/getproductimagebykodeid/" + selected_opt);
             xhr.setRequestHeader("Authorization", "Basic YWtiYXI6d2lyYWlzeQ==");
             xhr.responseType = 'blob';
             xhr.send();
