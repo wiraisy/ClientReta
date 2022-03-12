7fff15864de1285f0c6cb51e4668c6a27a9167af
stephanus yogi <stephanusyogi12@gmail.com>
2022-03-12 10:46:46 +0700

    New Progress

--- admin/application/controllers/Produkumum.php
+++ admin/application/controllers/Produkumum.php
@@ -172,7 +172,7 @@ class Produkumum extends MY_Controller {
 		list($width, $height, $type, $attr) = getimagesize($_FILES['img-produk']['tmp_name']);
 		
 		// Move uploaded file to a temp location
-		$uploadDir = 'uploads/';
+		$uploadDir = $_SERVER['DOCUMENT_ROOT'].'/admin/assets/uploads/';
 		$uploadFile = $uploadDir . basename($_FILES['img-produk']['name']);
 
 		var_dump($uploadFile);
